5a1a3ecf6f01d59da54c49527224a0b8f1046f13
ademblon <62746143+ademblon@users.noreply.github.com>
2023-10-04 13:48:25 +0200

    Chapter 4 : Rays, Camera and Background

--- main.cpp
+++ main.cpp
@@ -1,18 +1,47 @@
 #include <iostream>
 #include <fstream>
-#include "color.h"
-#include "vec3.h"
+#include "headers/color.h"
+#include "headers/vec3.h"
+#include "headers/ray.h"
 
 using namespace std;
+
+color ray_color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    double a = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+}
 
 int main() {
 
     ofstream Image("image.ppm");
 
     // Image
+    double aspect_ratio = 16.0/9.0;
+    int image_width = 400;
 
-    int image_width = 256;
-    int image_height = 256;
+    // Calculate the image height, and ensure that it's at least 1.
+    int image_height = static_cast<int>(image_width / aspect_ratio);
+    image_height = (image_height < 1 ) ? 1 : image_height;
+
+
+    // Camera
+    double focal_length = 1.0;
+    double viewport_height = 2.0;
+    double viewport_width = viewport_height * (static_cast<double>(image_width)/image_height);
+    point3 camera_center = point3(0,0,0);
+
+    // Calculate the vectors across the horizontal and down the vertical viewport edges.
+    vec3 viewport_u = vec3(viewport_width, 0, 0);
+    vec3 viewport_v = vec3(0, -viewport_height, 0);
+
+    // Calculate the horizontal and vertical delta vectors from pixel to pixel.
+    vec3 pixel_delta_u = viewport_u / image_width;
+    vec3 pixel_delta_v = viewport_v / image_height;
+
+    // Calculate the location of the upper left pixel.
+    vec3 viewport_upper_left = camera_center - vec3(0, 0, focal_length) - viewport_u/2 - viewport_v/2;
+    vec3 pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     // Render
 
@@ -21,10 +50,11 @@ int main() {
     for (int j = 0; j < image_height; ++j) {
         clog << "\rScanlines remaining: " << (image_height - j) << ' ' << flush;
         for (int i = 0; i < image_width; ++i) {
-            double r = double(i) / (image_width-1);
-            double g = double(j) / (image_height-1);
-            double b = 0;
-            color pixel_color(r,g,b);
+            point3 pixel_center = pixel00_loc + (i * pixel_delta_u) + (j * pixel_delta_v);
+            vec3 ray_direction = pixel_center - camera_center;
+            ray r(camera_center, ray_direction);
+
+            color pixel_color = ray_color(r);
             write_color(Image,pixel_color);
         }
     }
