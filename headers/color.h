cdab5f87ffa7dd8f28f7565acd5e9942959d7c0b
ademblon <62746143+ademblon@users.noreply.github.com>
2023-10-05 11:13:42 +0200

    Chapter 9 : Diffuse Materials and Gamma Correction

--- headers/color.h
+++ headers/color.h
@@ -8,6 +8,11 @@
 using namespace std;
 using color = vec3;
 
+inline double linear_to_gamma(double linear_component)
+{
+    return sqrt(linear_component);
+}
+
 void write_color(ofstream &Image, color pixel_color, int samples_per_pixel){
     static const interval intensity(0.0, 0.999);
 
@@ -20,6 +25,12 @@ void write_color(ofstream &Image, color pixel_color, int samples_per_pixel){
     r *= scale;
     g *= scale;
     b *= scale;
+
+    // Apply the linear to gamma transform.
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
+
 
     int cr = static_cast<int>(256*intensity.clamp(r));
     int cg = static_cast<int>(256*intensity.clamp(g));
