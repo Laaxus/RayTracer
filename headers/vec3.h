9995ce8570f56bce47041217e51a40cf45bbcc02
ademblon <62746143+ademblon@users.noreply.github.com>
2023-10-05 12:21:37 +0200

    Chapter 11 : Dielectrics Chapter 12 : Positionable Camera Chapter 13 : Defocus Blur (Depth of Field)

--- headers/vec3.h
+++ headers/vec3.h
@@ -106,6 +106,14 @@ inline vec3 unit_vector(vec3 v) {
     return v / v.length();
 }
 
+inline vec3 random_in_unit_disk() {
+    while (true) {
+        auto p = vec3(random_double(-1,1), random_double(-1,1), 0);
+        if (p.length_squared() < 1)
+            return p;
+    }
+}
+
 inline vec3 random_in_unit_sphere() {
     while (true) {
         vec3 p = vec3::random(-1,1);
@@ -118,6 +126,7 @@ inline vec3 random_unit_vector() {
     return unit_vector(random_in_unit_sphere());
 }
 
+
 inline vec3 random_on_hemisphere(const vec3& normal) {
     vec3 on_unit_sphere = random_unit_vector();
     if (dot(on_unit_sphere, normal) > 0.0) // In the same hemisphere as the normal
@@ -128,6 +137,13 @@ inline vec3 random_on_hemisphere(const vec3& normal) {
 
 vec3 reflect(const vec3& v, const vec3& n) {
     return v - 2*dot(v,n)*n;
+}
+
+inline vec3 refract(const vec3& uv, const vec3& n, double etai_over_etat) {
+    double cos_theta = fmin(dot(-uv, n), 1.0);
+    vec3 r_out_perp =  etai_over_etat * (uv + cos_theta*n);
+    vec3 r_out_parallel = -sqrt(fabs(1.0 - r_out_perp.length_squared())) * n;
+    return r_out_perp + r_out_parallel;
 }
 
 
