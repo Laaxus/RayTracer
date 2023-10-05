d0c5f70d9d7876211d6db8749f2cb6c77fa7fe71
ademblon <62746143+ademblon@users.noreply.github.com>
2023-10-05 11:40:54 +0200

    Chapter 10 : Metal and Materials

--- headers/hittable.h
+++ headers/hittable.h
@@ -2,12 +2,15 @@
 #define RAYTRACER_HITTABLE_H
 
 #include "ray.h"
+#include "rtweekend.h"
 
+class material;
 
 class hit_record {
 public:
     point3 p;
     vec3 normal;
+    shared_ptr<material> mat;
     double t;
 
     bool front_face;
--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "headers/rtweekend.h"
 
 #include "headers/color.h"
+#include "headers/material.h"
 #include "headers/hittable.h"
 #include "headers/hittable_list.h"
 #include "headers/sphere.h"
@@ -27,9 +28,15 @@ int main() {
 
     // World
     hittable_list world;
+    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+    auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
+    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8),0.3);
+    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2),1.0);
 
-    world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
-    world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
+    world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
+    world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
+    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
+    world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
 
     // Render
     camera cam;
